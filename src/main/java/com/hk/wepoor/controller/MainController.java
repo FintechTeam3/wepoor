9f7217cfd8203bbfc0bd8ee7f07c01831339a8b1
parkpirate <skbsy153@gmail.com>
2023-06-01 01:46:53 +0900

    메인화면 다시 설정_2023.06.01

--- src/main/java/com/hk/wepoor/controller/MainController.java
+++ src/main/java/com/hk/wepoor/controller/MainController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
 	@GetMapping("/category")
 	public String category() {
 		return "category";
