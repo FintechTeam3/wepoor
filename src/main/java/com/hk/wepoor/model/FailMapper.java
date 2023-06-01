dcd808dcda0c77073e2fbd708609b7f20c298609
hi0606 <hi0606@kakao.com>
2023-06-01 11:08:50 +0900

    삭제 CRUD TEST

--- src/main/java/com/hk/wepoor/model/FailMapper.java
+++ src/main/java/com/hk/wepoor/model/FailMapper.java
@@ -10,7 +10,11 @@ import com.hk.wepoor.vo.FailVO;
 @Mapper
 public interface FailMapper {
 	
-	List<FailVO> selectAll();
+	List<FailVO> selectAll(); // 전체조회	
 	
-	int insert(FailVO failVO);
+	int insert(FailVO failVO); //삽입 (실패 입력 행 몇개야?)
+	
+	int delete(int fail_id); //삭제
+	
+	
 }
--- src/main/java/com/hk/wepoor/service/FailService.java
+++ src/main/java/com/hk/wepoor/service/FailService.java
@@ -16,13 +16,20 @@ public class FailService {
 	@Autowired
 	FailMapper failmapper;
 	
+	// 전체조회
 	public List<FailVO> selectAll() {
 		List<FailVO> list = failmapper.selectAll();
 		return list;
 	}
-	
+	// 실패 입력
 	public int create(FailVO failVO) {
-		int failCount=failmapper.insert(failVO);
+		int failCount=failmapper.insert(failVO); //실패 입력 행 몇개야?
 		return failCount;
+	}
+	
+	// 실패 삭제 //삭제 갯수 몇개야 fail_id=7인 거 삭제해 -> 근데 그게 몇개야?
+	public int delete(int fail_id) {
+		int failDeleteCount=failmapper.delete(fail_id);
+		return failDeleteCount;
 	}
 }
