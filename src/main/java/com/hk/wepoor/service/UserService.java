6f0e05c79149bc65568a97b88acef643c7526609
ko <kokoko12334>
2023-06-05 17:34:34 +0900

    1233212

--- src/main/java/com/hk/wepoor/service/UserService.java
+++ src/main/java/com/hk/wepoor/service/UserService.java
@@ -1,9 +1,6 @@
 package com.hk.wepoor.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.hk.wepoor.model.UserMapper;
 
 @Service
 public class UserService {
