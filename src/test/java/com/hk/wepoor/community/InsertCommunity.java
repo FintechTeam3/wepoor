14e2cbd9065dc6b3506347d4bfbb51f156df1240
parkpirate <skbsy153@gmail.com>
2023-06-05 13:53:47 +0900

    community test 수정

--- src/test/java/com/hk/wepoor/community/InsertCommunity.java
+++ src/test/java/com/hk/wepoor/community/InsertCommunity.java
@@ -17,7 +17,7 @@ class InsertCommunity {
 	void contextLoads() {
 		
 		CommunityVO communityVO = null;
-		communityVO = new CommunityVO(0, 7, 2, "반갑습니다!");
+		communityVO = new CommunityVO(0, 7, 2, "반갑습니다!", null);
 		
 		int affectRowCount = community_service.create(communityVO);
 		
