fd65e0c2c989a07a22f9a3644a41b432bd3edb17
parkpirate <skbsy153@gmail.com>
2023-06-11 01:46:57 +0900

    구매 구현 완료

--- src/main/java/com/hk/wepoor/controller/PayController.java
+++ src/main/java/com/hk/wepoor/controller/PayController.java
@@ -1,14 +1,64 @@
 package com.hk.wepoor.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.hk.wepoor.model.UserMapper;
+import com.hk.wepoor.service.CategoryService;
+import com.hk.wepoor.service.PayService;
+import com.hk.wepoor.vo.CategoryVO;
+import com.hk.wepoor.vo.PayVO;
+import com.hk.wepoor.vo.UserVO;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PayController {
 	
+	@Autowired
+	CategoryService categoryService;
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	@Autowired
+	PayService payService;
+	
+	// 구매 - 혜정
 	@GetMapping("paydetail")
-	public String paydetail() {
+	public String paydetail(int cate_id, Model model, HttpServletRequest req) {
+		
+		CategoryVO cateVO = categoryService.select(cate_id);
+		model.addAttribute("category", cateVO);
+		
+		HttpSession session = req.getSession(false);
+		int userNo = (int) session.getAttribute("userNo");
+		UserVO userVO = userMapper.getUserByUserNo(userNo);
+		req.setAttribute("userPoint", userVO.getUserPoint());
+		req.setAttribute("userName", userVO.getUserName());
+		
 		return "paydetail";
 	}
 	
+	// 구매 완료 - 혜정
+	@GetMapping("paycomplete")
+	public String paycomplete(PayVO payVO, Model model) {
+		payService.create(payVO);
+		
+		CategoryVO cateVO = categoryService.select(payVO.getCate_id());
+		model.addAttribute("category", cateVO);
+		
+		int pId = payVO.getPay_id();
+		PayVO pay = payService.selectPayComplete(pId);
+		model.addAttribute("pay", pay);
+		
+		UserVO userVO = userMapper.getUserByUserNo(payVO.getUser_no());
+		model.addAttribute("user", userVO);
+		
+		return "paycomplete";
+	}
 }
