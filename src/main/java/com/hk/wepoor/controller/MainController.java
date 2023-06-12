6062781c65616e82663267bc3b5e5c2d27f3e55e
parkpirate <skbsy153@gmail.com>
2023-06-12 14:19:46 +0900

    0원 결제 구현, 기프트샵 구현중

--- src/main/java/com/hk/wepoor/controller/MainController.java
+++ src/main/java/com/hk/wepoor/controller/MainController.java
@@ -1,10 +1,21 @@
 package com.hk.wepoor.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hk.wepoor.model.UserMapper;
+import com.hk.wepoor.vo.UserVO;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	UserMapper userMapper;
 	
 	// 메인페이지
 	@GetMapping("/")
@@ -22,25 +33,25 @@ public class MainController {
 		return "roomMake";
 	}
 	
-	@GetMapping("/contact")
-	public String contact() {
-		return "contact";
-	}
-	
 	@GetMapping("/services")
 	public String services() {
 		return "services";
-	}
-	
-	@GetMapping("/teashop")
-	public String teashop() {
-		return "teashop";
 	}
 	
 	// 페이지 프레임
 	@GetMapping("/page")
 	public String page() {
 		return "page";
+	}
+	
+	// 기프트 카드
+	@GetMapping("/gift")
+	public String gift(HttpServletRequest req, Model model) {
+		HttpSession session = req.getSession(false);
+		int userNo = (int) session.getAttribute("userNo");
+		UserVO userVO = userMapper.getUserByUserNo(userNo);
+		model.addAttribute("userNickname", userVO.getUserNickname());
+		return "gift";
 	}
 	
 }
